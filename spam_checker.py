294e81fa2109a45fb5557d0e408ed7a68cee3d78
Lalith Prem <107936722+lalithtrs@users.noreply.github.com>
2023-09-13 21:12:16 +0530

    Update spam_checker.py

--- spam_checker.py
+++ spam_checker.py
@@ -1,18 +1,25 @@
 from mail_spam_checker import model, feature_extraction
 import pickle
+from langdetect import detect
+
 
 model_file = 'model.pkl'
 pickled_model = pickle.load(open('model.pkl', 'rb'))
 
-#   Building a Predictable System
-input_mail = [(input("enter your email "))]
-# Convert Text to feature vectors
-input_data_feature = feature_extraction.transform(input_mail)
+input_message = input("enter your email Z")
 
-# Making Prediction
-prediction = pickled_model.predict(input_data_feature)
+test = detect(input_message)
 
-print(prediction)
+if test == 'en' or test == 'ta':
+    input_mail = [input_message]
+    # Convert Text to feature vectors
+    input_data_feature = feature_extraction.transform(input_mail)
 
+    # Making Prediction
+    prediction = pickled_model.predict(input_data_feature)
+    print(prediction)
+    print("Message is valid")
 
+else:
+    print('0')
 
